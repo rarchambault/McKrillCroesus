c676360d47fb25ec3b16d3dc572d01ed691fc0aa
Emile Riberdy <emileriberdy@gmail.com>
2023-03-18 19:00:03 -0400

    more

--- dumb.py
+++ dumb.py
@@ -2,4 +2,5 @@ import finance_client as fc
 
 data = fc.getData(["AAL", "DAL", "UAL", "LUV", "HA"], "2023-01-01", "2023-02-01")
 
-print(data["AAL"])
+
+print(data["AAL"]["2023-01-03"])
--- finance_client.py
+++ finance_client.py
@@ -5,6 +5,7 @@ def getData(string, startDate, endDate):
     data = {}
     for i in string:
         data[i] = yf.download(i, start=startDate, end=endDate, ignore_tz=False)
+        data[i] = data[i]["Open"]
     return data 
 
 
